d2c74b6d9fcad5f77873f66140727cd87e0cf07b
cd.t <chasedemaster.tech@gmail.com>
2024-05-06 18:40:58 -0500

    first

--- app/page.tsx
+++ app/page.tsx
@@ -12,10 +12,11 @@ export default function Page() {
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
           <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
-            <a href="https://nextjs.org/learn/" className="text-blue-500">
+            <a href="https://nextjs.org/learn/" className="text-slate-500">
               Next.js Learn Course
             </a>
-            , brought to you by Vercel.
+            , brought to you by Vercel. 
+          <div className="h-0 w-0 border-b-[30px] border-l-[30px] border-r-[5px] border-b-black border-l-transparent border-r-transparent" /> 
           </p>
           <Link
             href="/login"
